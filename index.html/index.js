488b292134925a39bbdeca4b2a21ba0ac5c61973
Ladyperpetua <perpetuaoghwono@gmail.com>
2023-04-13 11:47:04 +0100

    first commit

---
+++ index.html/index.js
@@ -0,0 +1,10 @@
+const list = [
+  { id: 1, name: "renew gym membership" },
+  { id: 2, name: "go to church" },
+  { id: 1, name: "get groceries" },
+  { id: 1, name: "attend meeting" },
+  { id: 1, name: "take kim to school" },
+];
+
+for (val of list ) {console.log (val)
+}
